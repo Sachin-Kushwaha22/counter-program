a38fbd86d6e7eea26b22e355ef8d1208a1573330
Rachet2219 <sachin8n@gmail.com>
2024-07-03 12:06:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const increseBtn=document.getElementById("increseBtn");
+const resetBtn=document.getElementById("resetBtn");
+const decreaseBtn=document.getElementById("decreaseBtn");
+const countLabel=document.getElementById("countLabel");
+
+let count=0;
+
+increseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count=0;
+    countLabel.textContent = count;
+}
+
+decreaseBtn.onclick=function(){
+    count--;
+    countLabel.textContent = count;
+}
